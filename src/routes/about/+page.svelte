96450e51ea988ccef6dde7b721c2e1c4d18f7812
pwrstudio <pwrpntr@gmail.com>
2023-09-29 14:15:39 +0200

    feat: about page design

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,23 +1,8 @@
 <script lang="ts">
-	import Content from '$lib/components/page/Content.svelte';
+	import About from '$lib/components/page/About.svelte';
 	export let data;
 	const { about } = data;
 	console.log(data);
 </script>
 
-<div class="frontpage">
-	<div>About</div>
-	<div class="content">
-		<Content post={about} />
-	</div>
-</div>
-
-<style lang="scss">
-	.frontpage {
-		min-height: 100vh;
-		width: 900px;
-		margin-right: 0;
-		margin-left: 0;
-		padding: var(--default-padding);
-	}
-</style>
+<About post={about} />
